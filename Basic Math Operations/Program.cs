96d1fbfd06eefad2eaff89544fff2f5542ba8447
ARNAB SAHA <arnabsaha1999@gmail.com>
2021-02-01 10:26:28 +0600

    Basic Math Operations

---
+++ Basic Math Operations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Math_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20, y = 10;
+            Console.Write("Z=");
+            int z = int.Parse(Console.ReadLine());
+            Console.Write("Sum of three number:");
+            int sumoftwonumber = x + y + z;
+            Console.WriteLine(sumoftwonumber);
+            Console.Write("Subtraction of two number:");
+            int subtractionoftwonumber = x - y;
+            Console.WriteLine(subtractionoftwonumber);
+            Console.Write("Multiplication of two number:");
+            int multipleoftwonumber = x * y;
+            Console.WriteLine(multipleoftwonumber);
+            Console.Write("Division of two number:");
+            int Divisionoftwonumber = x / y;
+            Console.WriteLine(Divisionoftwonumber);
+
+        }
+    }
+}
