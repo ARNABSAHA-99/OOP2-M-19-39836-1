e73f7db79ee930add2550b81eb21c8964efd5668
ARNAB SAHA <arnabsaha1999@gmail.com>
2021-02-15 11:29:58 +0600

    LabTask-4.2

--- Lab Task-4/Program.cs
+++ Lab Task-4/Program.cs
@@ -10,6 +10,7 @@ namespace Lab_Task_4
     {
         static void Main(string[] args)
         {
+            //1
             int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
             Console.Write("Reverse value array1: ");
@@ -20,12 +21,15 @@ namespace Lab_Task_4
             Console.WriteLine();
 
 
+            //2
             int sum = 0;
             for (int i = 0; i < array1.Length; i++)
             {
                 sum = sum + array1[i];
             }
             Console.WriteLine("Sum of the all array element: "+sum);
+
+            //3
 
             int[] array2 = new int[8];
 
@@ -38,6 +42,32 @@ namespace Lab_Task_4
             for (int n = 0; n < array2.Length; n++)
             {
                 Console.Write(array2[n] + " ");
+            }
+            Console.WriteLine();
+
+            //4
+
+            int[] array3 = { 1, 1, 2, 3, 4, 4, 5, 6, 6, 7,10,10,149,149 };
+            for (int z = 0; z < array3.Length; z++)
+            {
+                for (int y = z + 1; y < array3.Length; y++)
+                {
+                    if (array3[z] == array3[y])
+                        Console.WriteLine("{0} has 2 duplication.", array3[z]);
+                }
+            }
+            Console.WriteLine();
+
+            //5
+
+            int[] array4 = { 1,2,3,4,5 };
+            for (int w = 0; w < array4.Length; w++)
+            {
+                for (int q = w + 1; q < array4.Length; q++)
+                {
+                    if (array3[w] != array4[q])
+                        Console.WriteLine("{0} is unique number.", array4[w]);
+                }
             }
             Console.WriteLine();
 
